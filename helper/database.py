a7bfc37c1e220d80c20b9f4643f9c6924557cad5
Bhimsinh Sodha <119826787+bjsodha@users.noreply.github.com>
2023-03-06 15:27:53 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo 
 import os
 from helper.date import add_date
-DB_NAME = os.environ.get("DB_NAME","")
-DB_URL = os.environ.get("DB_URL","")
+DB_NAME = os.environ.get("DB_NAME","RENAMEBOTV3")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Jivrajsinh:Jivrajsinh@cluster0.cllmdjd.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
